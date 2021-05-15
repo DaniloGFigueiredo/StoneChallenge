2c44abaf889b785604b7b253623ff5796994290a
daanmuca16 <43374350+daanmuca16@users.noreply.github.com>
2021-05-15 15:08:29 -0300

    build: add controllers and Employee solution basic structures

---
+++ Employee.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.Api.Controllers
+{
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+    }
+}
---
+++ Employee.Domain/Enums/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Domain.Enums
+{
+    public class Enum
+    {
+       public enum EmployeeArea
+        {
+            BoardOfDirectors = 0,
+            Accounting = 1,
+            GeneralServices = 2,
+            CustomerRelations = 3
+        }
+    }
+}
